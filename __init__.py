8cb76c04ad9744f2d0f780626a93b7423574d804
Jackey1999 <48642847+Jackey1999@users.noreply.github.com>
2019-04-16 01:48:27 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# Author  : Jakcey1999
+# Email   : 9516694 # qq.com
+# Time    : 2019-04-01-001 上午 9:04
+# File    : __init__.py.py
+# IDE     : PyCharm [Python 3.x]
+# Code is far away from bugs with the god animal protecting.
+  Life is short. You need Python 3.x
+
+"""
