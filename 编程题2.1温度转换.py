aa5dfe49b650e295330295409ee5d827494a699c
Jackey1999 <48642847+Jackey1999@users.noreply.github.com>
2019-04-16 01:41:53 +0800

    Add files via upload

---
+++ 编程题2.1温度转换.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# Author  : Jakcey1999
+# Email   : 9516694 # qq.com
+# Time    : 2019-03-20-020 下午 13:43
+# File    : 编程题2.1温度转换.py
+# IDE     : PyCharm [Python 3.x]
+# Code is far away from bugs with the god animal protecting.
+  Life is short. You need Python 3.x
+
+"""
+
+# PDF p61, Book p45
+
+# 2.1
+
+celsius = eval(input("请输入摄氏温度="))
+fahreheit = (9 / 5) * celsius + 32
+print("转换后的华氏温度=", fahreheit)
